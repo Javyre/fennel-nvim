ee822a5b7af1db47bb82c79de540db7517306d35
Javier A. Pollak <javi.po.123@gmail.com>
2021-04-12 10:52:05 -0400

    Fix module 'fennel' not found error in fennelview
    Not sure if this was only an issue on neovim-0.5...

--- lua/fennel-nvim/lib/fennelview.lua
+++ lua/fennel-nvim/lib/fennelview.lua
@@ -1,7 +1,7 @@
 local function view_quote(str)
   return ("\"" .. str:gsub("\"", "\\\"") .. "\"")
 end
-require("fennel").metadata:setall(view_quote, "fnl/arglist", {"str"})
+require("fennel-nvim.lib.fennel").metadata:setall(view_quote, "fnl/arglist", {"str"})
 local short_control_char_escapes = {["\11"] = "\\v", ["\12"] = "\\f", ["\13"] = "\\r", ["\7"] = "\\a", ["\8"] = "\\b", ["\9"] = "\\t", ["\n"] = "\\n"}
 local long_control_char_escapes = nil
 do
@@ -20,11 +20,11 @@ local function escape(str)
   local str = str:gsub("(%c)%f[0-9]", long_control_char_escapes)
   return str:gsub("%c", short_control_char_escapes)
 end
-require("fennel").metadata:setall(escape, "fnl/arglist", {"str"})
+require("fennel-nvim.lib.fennel").metadata:setall(escape, "fnl/arglist", {"str"})
 local function sequence_key_3f(k, len)
   return ((type(k) == "number") and (1 <= k) and (k <= len) and (math.floor(k) == k))
 end
-require("fennel").metadata:setall(sequence_key_3f, "fnl/arglist", {"k", "len"})
+require("fennel-nvim.lib.fennel").metadata:setall(sequence_key_3f, "fnl/arglist", {"k", "len"})
 local type_order = {["function"] = 5, boolean = 2, number = 1, string = 3, table = 4, thread = 7, userdata = 6}
 local function sort_keys(a, b)
   local ta = type(a)
@@ -45,7 +45,7 @@ local function sort_keys(a, b)
     end
   end
 end
-require("fennel").metadata:setall(sort_keys, "fnl/arglist", {"a", "b"})
+require("fennel-nvim.lib.fennel").metadata:setall(sort_keys, "fnl/arglist", {"a", "b"})
 local function get_sequence_length(t)
   local len = 1
   for i in ipairs(t) do
@@ -53,7 +53,7 @@ local function get_sequence_length(t)
   end
   return len
 end
-require("fennel").metadata:setall(get_sequence_length, "fnl/arglist", {"t"})
+require("fennel-nvim.lib.fennel").metadata:setall(get_sequence_length, "fnl/arglist", {"t"})
 local function get_nonsequential_keys(t)
   local keys = {}
   local sequence_length = get_sequence_length(t)
@@ -65,7 +65,7 @@ local function get_nonsequential_keys(t)
   table.sort(keys, sort_keys)
   return keys, sequence_length
 end
-require("fennel").metadata:setall(get_nonsequential_keys, "fnl/arglist", {"t"})
+require("fennel-nvim.lib.fennel").metadata:setall(get_nonsequential_keys, "fnl/arglist", {"t"})
 local function count_table_appearances(t, appearances)
   if (type(t) == "table") then
     if not appearances[t] then
@@ -82,7 +82,7 @@ local function count_table_appearances(t, appearances)
   end
   return appearances
 end
-require("fennel").metadata:setall(count_table_appearances, "fnl/arglist", {"t", "appearances"})
+require("fennel-nvim.lib.fennel").metadata:setall(count_table_appearances, "fnl/arglist", {"t", "appearances"})
 local put_value = nil
 local function puts(self, ...)
   for _, v in ipairs({...}) do
@@ -90,15 +90,15 @@ local function puts(self, ...)
   end
   return nil
 end
-require("fennel").metadata:setall(puts, "fnl/arglist", {"self", "..."})
+require("fennel-nvim.lib.fennel").metadata:setall(puts, "fnl/arglist", {"self", "..."})
 local function tabify(self)
   return puts(self, "\n", (self.indent):rep(self.level))
 end
-require("fennel").metadata:setall(tabify, "fnl/arglist", {"self"})
+require("fennel-nvim.lib.fennel").metadata:setall(tabify, "fnl/arglist", {"self"})
 local function already_visited_3f(self, v)
   return (self.ids[v] ~= nil)
 end
-require("fennel").metadata:setall(already_visited_3f, "fnl/arglist", {"self", "v"})
+require("fennel-nvim.lib.fennel").metadata:setall(already_visited_3f, "fnl/arglist", {"self", "v"})
 local function get_id(self, v)
   local id = self.ids[v]
   if not id then
@@ -109,7 +109,7 @@ local function get_id(self, v)
   end
   return tostring(id)
 end
-require("fennel").metadata:setall(get_id, "fnl/arglist", {"self", "v"})
+require("fennel-nvim.lib.fennel").metadata:setall(get_id, "fnl/arglist", {"self", "v"})
 local function put_sequential_table(self, t, len)
   puts(self, "[")
   self.level = (self.level + 1)
@@ -120,7 +120,7 @@ local function put_sequential_table(self, t, len)
   self.level = (self.level - 1)
   return puts(self, " ]")
 end
-require("fennel").metadata:setall(put_sequential_table, "fnl/arglist", {"self", "t", "len"})
+require("fennel-nvim.lib.fennel").metadata:setall(put_sequential_table, "fnl/arglist", {"self", "t", "len"})
 local function put_key(self, k)
   if ((type(k) == "string") and k:find("^[-%w?\\^_!$%&*+./@:|<=>]+$")) then
     return puts(self, ":", k)
@@ -128,7 +128,7 @@ local function put_key(self, k)
     return put_value(self, k)
   end
 end
-require("fennel").metadata:setall(put_key, "fnl/arglist", {"self", "k"})
+require("fennel-nvim.lib.fennel").metadata:setall(put_key, "fnl/arglist", {"self", "k"})
 local function put_kv_table(self, t, ordered_keys)
   puts(self, "{")
   self.level = (self.level + 1)
@@ -148,7 +148,7 @@ local function put_kv_table(self, t, ordered_keys)
   tabify(self)
   return puts(self, "}")
 end
-require("fennel").metadata:setall(put_kv_table, "fnl/arglist", {"self", "t", "ordered-keys"})
+require("fennel-nvim.lib.fennel").metadata:setall(put_kv_table, "fnl/arglist", {"self", "t", "ordered-keys"})
 local function put_table(self, t)
   if (already_visited_3f(self, t) and self["detect-cycles?"]) then
     return puts(self, "#<table ", get_id(self, t), ">")
@@ -168,7 +168,7 @@ local function put_table(self, t)
     end
   end
 end
-require("fennel").metadata:setall(put_table, "fnl/arglist", {"self", "t"})
+require("fennel-nvim.lib.fennel").metadata:setall(put_table, "fnl/arglist", {"self", "t"})
 local function _0_(self, v)
   local tv = type(v)
   if (tv == "string") then
@@ -181,13 +181,13 @@ local function _0_(self, v)
     return puts(self, "#<", tostring(v), ">")
   end
 end
-require("fennel").metadata:setall(_0_, "fnl/arglist", {"self", "v"})
+require("fennel-nvim.lib.fennel").metadata:setall(_0_, "fnl/arglist", {"self", "v"})
 put_value = _0_
 local function one_line(str)
   local ret = str:gsub("\n", " "):gsub("%[ ", "["):gsub(" %]", "]"):gsub("%{ ", "{"):gsub(" %}", "}"):gsub("%( ", "("):gsub(" %)", ")")
   return ret
 end
-require("fennel").metadata:setall(one_line, "fnl/arglist", {"str"})
+require("fennel-nvim.lib.fennel").metadata:setall(one_line, "fnl/arglist", {"str"})
 local function fennelview(x, options)
   local options = (options or {})
   local inspector = inspector
@@ -209,5 +209,5 @@ local function fennelview(x, options)
     end
   end
 end
-require("fennel").metadata:setall(fennelview, "fnl/arglist", {"x", "options"}, "fnl/docstring", "Return a string representation of x.")
+require("fennel-nvim.lib.fennel").metadata:setall(fennelview, "fnl/arglist", {"x", "options"}, "fnl/docstring", "Return a string representation of x.")
 return fennelview
